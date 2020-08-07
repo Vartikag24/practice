e320e02ddfe01718e130a921c88e0c38a5c12acf
Vartika G <vartikag.2424@gmail.com>
2020-08-07 11:09:23 +0530

    lec1 done

---
+++ dart/lec1.dart
@@ -0,0 +1,17 @@
+void main() {
+  
+  String fName = 'Vartika';
+  var lName='Gupta';  
+  
+  int age=20;
+  var houseNo=402; 
+ 
+  bool isEmployed =false;
+  
+  print(fName);
+  print(lName);
+  print(age);
+  print(houseNo);
+  print(isEmployed);
+   
+}
